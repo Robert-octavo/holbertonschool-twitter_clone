1fde32a154e8e6993552257dda6d1ad84d69f8a2
Robert-octavo <robert.ortega.octavo@gmail.com>
2023-08-01 23:15:37 -0500

    test

---
+++ twitter/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:twitter/screens/signin_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Hello World!'),
+//         ),
+//       ),
+//     );
+//   }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SignIn(),
+    );
+  }
+}
